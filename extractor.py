4153d5081a2f8a4f10805376407db1e800ef4b33
verteramo <49007909+verteramo@users.noreply.github.com>
2023-04-26 23:56:38 +0200

    Scrapper changes

--- extractor.py
+++ extractor.py
@@ -1,7 +1,31 @@
-from pdfminer.high_level import extract_pages
-from pdfminer.layout import LTTextContainer
+import re
+from PyPDF2 import PdfReader
 
-for page_layout in extract_pages("Cuestionarios.pdf"):
-    for element in page_layout:
-        if isinstance(element, LTTextContainer):
-            print(element.get_text())
+reader = PdfReader("Cuestionarios.pdf")
+
+re_questions = r"(?:Se puntúa \d{1,2},\d{2} sobre \d{1,2},\d{2}(?!\n)|(?:La respuesta correcta es:?[^.]*?[.'VF]\n|Las respuestas correctas son: [\S\s]+?\.\n)(?!Pregunta|Actividad|\d{1,2}/\d{1,2}/\d{2}))(?P<question>(?:(?:[^…:?]+)[…:?])+?)\n(?P<answers>[\S\s]+?)(?=La respuesta correcta es|Las respuestas correctas son)"
+re_right_answers = r"(?:La respuesta correcta es:? |Las respuestas correctas son: )(?:'?(?P<answer>[\S\s]*?\.?)[\n'])"
+PATTERN_ANSWER_CLEAN = re.compile(r"^(?:[a-z]\.)?(?P<answer>.+)$")
+
+PATTERN_ANSWER_SPLIT = re.compile(r"\.\n|\.\uf00c\n|\.\uf00d\n$")
+
+for page in reader.pages:
+    text = page.extract_text()
+    questions = re.findall(re_questions, text)
+    correct_answers = re.findall(re_right_answers, text)
+    for question, answers in questions:
+        question = str(question).replace("\n\r", " ").rstrip(":")
+
+        answers = [
+            answer.group("answer")
+            for answer in [
+                PATTERN_ANSWER_CLEAN.search(answer.replace("\n", " "))
+                for answer in PATTERN_ANSWER_SPLIT.split(answers)
+            ]
+            if answer is not None
+        ]
+
+        print(f"Q: {question}")
+        for answer in answers:
+            print(answer)
+        print()
--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,4 @@
-# Copyright 2023 @verteramo
+# @verteramo
 
 import io, argparse, re, json
 from selenium import webdriver
@@ -7,137 +7,151 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
+# Ordered answers extraction
+# Example: "a. Texto de la respuesta." -> "Texto de la respuesta."
 PATTERN_MATCH_ANSWER = re.compile(r"^(?:[a-z]\.\s)?(?P<answer>.+)$")
 
+# Right answers extraction
+# Example: "Las respuestas correctas son: Respuesta 1., Respuesta 2." -> "Respuesta 1., Respuesta 2."
 PATTERN_MATCH_RIGHT_ANSWER = re.compile(
     r"^(?:(?:La respuesta correcta es:?|Las respuestas correctas son:)\s)(?P<rightanswer>.+)$"
 )
 
 
-# Cada pregunta tiene como elemento raíz un elemento de clase "content"
+# Questions' root element has "content" class
 class Question:
     def __init__(self, element: WebElement) -> None:
         self.__element = element
 
-    # Obtiene el texto de la pregunta
     def get_text(self) -> str:
-        return self.__element.find_element(By.CLASS_NAME, "qtext").text.rstrip(":")
+        try:
+            return self.__element.find_element(By.CLASS_NAME, "qtext").text.rstrip(":")
+        
+        # GVA v3.9.12
+        except NoSuchElementException:
+            return self.__element.find_element(
+                By.XPATH, "//*[@class='qtext']/p"
+            ).text.rstrip(":")
 
-    # Obtiene la respuesta o las respuestas correctas
     def __get_rightanswer(self) -> list[str] | str | None:
         try:
-            # Se obtiene la retroalimentación y se divide en una lista
-            rightanswer_list = (
-                PATTERN_MATCH_RIGHT_ANSWER.search(
-                    self.__element.find_element(By.CLASS_NAME, "rightanswer").text
+            rightanswer_list = list(
+                map(
+                    lambda rightanswer: rightanswer.strip("'").rstrip("."),
+                    (
+                        PATTERN_MATCH_RIGHT_ANSWER.search(
+                            self.__element.find_element(
+                                By.CLASS_NAME, "rightanswer"
+                            ).text
+                        )
+                        .group("rightanswer")
+                        .strip("'")
+                        .rstrip(".")
+                        .split("., ")
+                    ),
                 )
-                .group("rightanswer")
-                .strip("'")
-                .rstrip(".")
-                .split("., ")
             )
 
-            # Se eliminan las comillas y los puntos de cada respuesta
-            for position in range(len(rightanswer_list)):
-                rightanswer_list[position] = (
-                    rightanswer_list[position].strip("'").rstrip(".")
-                )
+            return (
+                rightanswer_list[0] if len(rightanswer_list) == 1 else rightanswer_list
+            )
+        
+        # GVA v3.9.12
+        except AttributeError:
+            return None
 
-            # Si la lista tiene una sola respuesta se devuelve la respuesta
-            if len(rightanswer_list) == 1:
-                return rightanswer_list[0]
-
-            # En caso contrario se devuelve la lista de respuestas
-            return rightanswer_list
-
-        # Si no hay retroalimentación se devuelve None
+        # No feedback
         except NoSuchElementException:
             return None
 
-    # Respuestas de tipo texto
+    # Text answers (input)
     def __get_text_answer(
         self, element: WebElement, rightanswer: list[str] | str | None
     ) -> str | None:
         text = element.find_element(By.TAG_NAME, "input").get_attribute("value")
 
-        # Si hay retroalimentación
         if rightanswer:
-            # Si es una lista y el texto está en la lista
             if isinstance(rightanswer, list) and text.casefold() in map(
                 str.casefold, rightanswer
             ):
                 return text
-            # Si es un string y el texto es igual al string
             elif text.casefold() == rightanswer.casefold():
                 return text
-        # Si no hay retroalimentación se intenta inferir de la corrección
+
+        # Inference
         elif "fa-check" in element.find_element(By.TAG_NAME, "i").get_attribute(
             "class"
         ):
             return text
-        # Si no hay retroalimentación y no hay corrección se devuelve None
+
         return None
 
-    # Respuestas de tipo checkbox o radio
+    # Multiple answers (checkbox and radio)
     def __get_multiple_answer(
         self, element: WebElement, rightanswer: list[str] | str | None
     ) -> list[tuple[str, bool]]:
         answers = []
-        for answer in element.find_elements(By.TAG_NAME, "div"):
+
+        # V3.9.12 not working with By.TAG_NAME, it works with By.XPATH
+        for answer in element.find_elements(By.XPATH, "div"):
             try:
-                # En v3.9.12 las respuestas están envueltas en un div
+                # v3.9.12 answers are wrapped in a div
                 text = answer.find_element(By.TAG_NAME, "div").text
             except NoSuchElementException:
-                # En v3.7.7 las respuestas están envueltas en un label
+                # v3.7.7 answers are wrapped in a label
                 text = answer.find_element(By.TAG_NAME, "label").text
 
             text = PATTERN_MATCH_ANSWER.search(text).group("answer").rstrip(".")
 
-            # Si hay respuesta correcta debe ser correcta o incorrecta
-            # Si la respuesta está entre las respuestas correctas
             if isinstance(rightanswer, list):
                 answers.append((text, text in rightanswer))
-            # Si es la respuesta correcta
+
             elif isinstance(rightanswer, str):
                 answers.append((text, text == rightanswer))
 
-            # Si no hay respuesta correcta se intenta inferir de la corrección
+            # Inference
             else:
                 classes = answer.get_attribute("class").split(" ")
-                # Si está marcada como correcta
+
                 if "correct" in classes:
                     answers.append((text, True))
-                # Si está marcada como incorrecta
+
                 elif "incorrect" in classes:
                     answers.append((text, False))
-                # No hay retroalimentación
+
                 else:
                     answers.append((text, None))
 
+        single = (
+            element.find_element(By.TAG_NAME, "input").get_attribute("type") == "radio"
+        )
+
+        # If it is a single answer and there is one correct answer, the rest are incorrect
+        if single and len([answer for answer, truth in answers if truth]) == 1:
+            answers = [
+                (answer, False if truth is None else truth) for answer, truth in answers
+            ]
+
         return answers
 
-    # Respuestas de tipo emparejamiento
+    # Matching answers (select)
     def __get_matching_answer(
         self, element: WebElement, rightanswer: list[str] | str | None
-    ) -> list[tuple[str, str]]:
+    ) -> list[tuple[str, str, bool]]:
         answers = []
         for row in element.find_elements(By.TAG_NAME, "tr"):
             text = row.find_element(By.CLASS_NAME, "text").text
 
-            # print(f"Debug({__name__}): {rightanswer}")
-
             rightanswer_dict = dict()
             if isinstance(rightanswer, list):
                 for answer in rightanswer:
-                    key, value = answer.split(" → ")
-                    key = key.strip()
-                    value = value.strip()
+                    key, value = list(map(str.strip, answer.split(" → ")))
                     rightanswer_dict[key] = (value, True)
+
             elif isinstance(rightanswer, str):
-                key, value = rightanswer.split(" → ")
-                key = key.strip()
-                value = value.strip()
+                key, value = list(map(str.strip, rightanswer.split(" → ")))
                 rightanswer_dict[key] = (value, True)
+
             else:
                 for option in row.find_elements(By.TAG_NAME, "option"):
                     if option.get_attribute("selected"):
@@ -158,15 +172,20 @@ class Question:
 
     def get_answer(self):
         rightanswer = self.__get_rightanswer()
-        answer_element = self.__element.find_element(By.CLASS_NAME, "answer")
+        try:
+            answer = self.__element.find_element(By.CLASS_NAME, "answer")
+        
+        # GVA v3.9.12
+        except NoSuchElementException:
+            answer = self.__element.find_element(By.XPATH, "//*[@class='answer']")
 
-        match answer_element.tag_name:
-            case "span":  # Respuestas de tipo texto (input text)
-                return self.__get_text_answer(answer_element, rightanswer)
-            case "div":  # Respuestas de selección múltiple (checkbox o radio)
-                return self.__get_multiple_answer(answer_element, rightanswer)
-            case "table":  # Respuestas de emparejamiento (select)
-                return self.__get_matching_answer(answer_element, rightanswer)
+        match answer.tag_name:
+            case "span":  # Text answers (input)
+                return self.__get_text_answer(answer, rightanswer)
+            case "div":  # Multiple answers (checkbox and radio)
+                return self.__get_multiple_answer(answer, rightanswer)
+            case "table":  # Matching answers (select)
+                return self.__get_matching_answer(answer, rightanswer)
         return None
 
 
@@ -175,11 +194,18 @@ class Test:
         self.__driver = driver
 
     def get_name(self) -> str:
-        return (
-            self.__driver.find_elements(By.CLASS_NAME, "breadcrumb-item")[-1]
-            .find_element(By.TAG_NAME, "a")
-            .text.rstrip(".")
-        )
+        try:
+            return (
+                self.__driver.find_elements(By.CLASS_NAME, "breadcrumb-item")[-1]
+                .find_element(By.TAG_NAME, "a")
+                .text.rstrip(".")
+            )
+        
+        # GVA v3.9.12
+        except IndexError:
+            return self.__driver.find_elements(
+                By.XPATH, "//ol[contains(@class, 'breadcrumb')]/li/span/a/span"
+            )[-1].text.rstrip(".")
 
     def get_questions(self) -> list[Question]:
         for element in self.__driver.find_elements(By.XPATH, "//*[@class='content']"):
@@ -189,17 +215,17 @@ class Test:
 class Platform:
     def __init__(self, driver: webdriver) -> None:
         parser = argparse.ArgumentParser(description="Scraps tests")
-        parser.add_argument("--uid", type=str, default="username")
-        parser.add_argument("--pid", type=str, default="password")
-        parser.add_argument("--bid", type=str, default="loginbtn")
-        parser.add_argument("--u", type=str)
-        parser.add_argument("--p", type=str)
-        parser.add_argument("--l", type=str)
+        parser.add_argument("-uid", "--username_id", type=str, default="username")
+        parser.add_argument("-pid", "--password_id", type=str, default="password")
+        parser.add_argument("-bid", "--loginbtn_id", type=str, default="loginbtn")
+        parser.add_argument("-l", "--links", type=str, default="links.txt")
+        parser.add_argument("-u", "--username", type=str)
+        parser.add_argument("-p", "--password", type=str)
         self.__args = parser.parse_args()
         self.__driver = driver
 
     def __get_links(self) -> list[str]:
-        with open(self.__args.l, "r") as links:
+        with open(self.__args.links, "r") as links:
             for link in links.readlines():
                 yield link
 
@@ -207,15 +233,16 @@ class Platform:
         for link in self.__get_links():
             self.__driver.get(link)
             try:
-                self.__driver.find_element(By.ID, self.__args.uid).send_keys(
-                    self.__args.u
+                self.__driver.find_element(By.ID, self.__args.username_id).send_keys(
+                    self.__args.username
                 )
-                self.__driver.find_element(By.ID, self.__args.pid).send_keys(
-                    self.__args.p
+                self.__driver.find_element(By.ID, self.__args.password_id).send_keys(
+                    self.__args.password
                 )
-                self.__driver.find_element(By.ID, self.__args.bid).click()
+                self.__driver.find_element(By.ID, self.__args.loginbtn_id).click()
             except:
                 pass
+
             yield self.__driver
 
     def get_tests(self) -> list[Test]:
@@ -241,15 +268,7 @@ def main():
     for test in Platform(get_driver()).get_tests():
         questions = []
         for question in test.get_questions():
-            answer = question.get_answer()
-            if isinstance(answer, list):
-                answers = []
-                for a, *t in answer:
-                    answers.append((a, t))
-            else:
-                answers = answer
-
-            questions.append((question.get_text(), answers))
+            questions.append((question.get_text(), question.get_answer()))
 
         try:
             tests[test.get_name()] += questions
